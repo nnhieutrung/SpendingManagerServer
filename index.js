aa96fd138d267c10b48ed2af6d4dc62785ab3320
nnhieutrung <67868869+nnhieutrung@users.noreply.github.com>
2022-05-20 03:27:48 +0700

    .

--- index.js
+++ index.js
@@ -5,8 +5,6 @@ const PORT = process.env.PORT || 5000
 const app = express();
 const MongoDB =  require('mongodb');
 const bodyParser = require('body-parser');
-const bcrypt = require('bcrypt');
-
 
 const config = require('./config.json');
 MongoClient = new MongoDB.MongoClient(encodeURI(config.mongodb) , { useUnifiedTopology: true } );
@@ -24,62 +22,241 @@ async function GetRandomString(LENGTHTEXT)
 
 
 async function GetRandomToken() {
-  let token = await GetRandomString(10);
+  let token = await GetRandomString(75);
   let checkToken = await MongoClient.db("main").collection("token").find({ token : token });
   if (checkToken.length > 0) return GetRandomToken();
 
   return token;
 }
 
-
-async function CheckToken(req, res, next) {
-  try {
-    let token = req.query.token
-    let checkToken = await MongoClient.db("main").collection("token").find({ token : token, expires: { $gt: now() } });
-    if (checkToken.length > 0) return next();
-  }
-  catch (e) {
-    res.status(404)
-  }
-
+async function GetUserDataFromToken(token) {
+  if (!token) return;
+  let checkToken = await MongoClient.db("main").collection("token").find({ token : token, expires: { $gt: Date.now() } }).toArray();
+  if (checkToken.length == 0) return;
+  let checkData = await MongoClient.db("main").collection("account").find({ username : checkToken[0].username}).toArray();
+  if (checkData.length == 0)  return;
+  return checkData[0]
 }
+
+
 
 
 async function main()
 {
+  process.env.TZ = "Asia/Ho_Chi_Minh";
+  console.log(`Started At : ${new Date().toLocaleString('vi-VN')}`)
+  await MongoClient.connect();
+  console.log("Database connected");
 
-    app
+  app
     .set('views', path.join(__dirname, 'public'))
     .set('view engine', 'ejs')
     .engine('html', require('ejs').renderFile)  
     .get("/ping", async (req, res) => {
       try {
-        res.json({data : "pong"})
+        res.json({data : Date.now()});
       }
       catch (e) {
-        console.log(err)
+        console.log(e)
+      }
+    })
+    .get("/login", async (req, res) => {
+      try {
+        console.log("WRONGG")
+        
+      }
+      catch (e) {
+        console.error(e)
+        return res.status(406).json({ error : "Có lỗi phát sinh trên máy chủ. Vui lòng thử lại"});
       }
     })
     .use(bodyParser())
+    // POST without token
     .post("/login", async (req, res) => {
       try {
-        let data = req.body.data;
-        let username = data.user;
-        let hashPass = data.password;
-        let userData = await MongoClient.db("main").collection("account").find({username: username, password: hashPass}).toArray()
-        if (userData.length > 0) {
-          let token = GetRandomToken()
-          MongoClient.db("main").collection("token").insertOne({username: username, token: token, expires: Date.now()  + 24*60*60*1000 })
+       
+        let body = req.body;
+        let data = await MongoClient.db("main").collection("account").find({username: body.username.toLowerCase(), password: body.password.toLowerCase()}).toArray()
+        if (data.length > 0) {
+          let token = await GetRandomToken()
+          MongoClient.db("main").collection("token").insertOne({username: body.username, token: token, expires: Date.now()  + 24*60*60*1000 })
+          console.log(`Create Token: ${token} for user ${body.username}`)
+          return res.status(200).json({ token : token})
         }
+
+        return res.status(200).json({ token : ""})
       }
       catch (e) {
-        return res.status(404);
+        console.error(e)
+        return res.status(406).json({ error : "Có lỗi phát sinh trên máy chủ. Vui lòng thử lại"});
       }
     })
     .post("/signup" , async (req, res) => {
+      let body = req.body;
+      body.username = (body.username || "").toLowerCase();
+      body.password = (body.password || "").toLowerCase();
+      body.createdOn = Date.now();
 
+      let data = await MongoClient.db("main").collection("account").find({username: body.username.toLowerCase()}).toArray()
+      if (data.length == 0) {
+        let token = await GetRandomToken()
+        MongoClient.db("main").collection("account").insertOne(body)
+        console.log(`Create Token: username ${body.username} for person name ${body.fullname}`)
+        return res.status(200).json({ message : "Bạn đã tạo tài khoản thành công"})
+      }
+      else  return res.status(200).json({ message : "Tên tài khoản đăng nhập hiện đã có vui lòng chọn tên khác"})
     })
-    .listen(PORT, () => console.info("WebApp" , `Listening on ${ PORT }`))
+    /// POST with Token
+    .post("/*", async (req, res, next) => {
+      try {
+        let token = req.headers['token-user']
+        let userData = await GetUserDataFromToken(token)
+        if (userData) {
+          req.userData = userData
+          console.log("Authenticated: ", userData)
+          next();
+        }
+          
+        else
+          res.status(401).json({ error: "Phiên đăng nhập không hợp lệ"});
+      }
+      catch (e) {
+        console.error(e)
+        res.status(406).json({ error : "Có lỗi phát sinh trên máy chủ. Vui lòng thử lại"});
+      }
+    })
+    // Some get data
+    .post("/account", async (req, res) => {
+      try {
+        let data = req.userData
+        delete data.password;
+        delete data._id;
+        console.log("Get Account: ", data)
+        res.status(200).json(data);
+      }
+      catch (e) {
+        console.error(e)
+        res.status(406).json({ error : "Có lỗi phát sinh trên máy chủ. Vui lòng thử lại"});
+      }
+    })
+    .post("/wallets", async (req, res) => {
+      try {
+        let username = req.userData.username
+        let data = await MongoClient.db("main").collection("wallet").find({username: username}).toArray()
+
+        for (let i = 0; i < data.length; i++) 
+          data[i].id = data[i]._id.toString()
+        
+        console.log(data)
+        res.status(200).json(data);
+      }
+      catch (e) {
+        console.error(e)
+        res.status(406).json({ error : "Có lỗi phát sinh trên máy chủ. Vui lòng thử lại"});
+      }
+    })
+    .post("/loans", async (req, res) => {
+      try {
+        let username = req.userData.username
+        let data = await MongoClient.db("main").collection("loan").find({username: username}).toArray()
+
+        for (let i = 0; i < data.length; i++) 
+          data[i].id = data[i]._id.toString()
+  
+        console.log(data)
+        res.status(200).json(data);
+      }
+      catch (e) {
+        console.error(e)
+        res.status(406).json({ error : "Có lỗi phát sinh trên máy chủ. Vui lòng thử lại"});
+      }
+    })
+    .post("/debts", async (req, res) => {
+      try {
+        let username = req.userData.username
+        let data = await MongoClient.db("main").collection("debt").find({username: username}).toArray()
+
+        for (let i = 0; i < data.length; i++) 
+          data[i].id = data[i]._id.toString()
+        console.log(data)
+        res.status(200).json(data);
+      }
+      catch (e) {
+        console.error(e)
+        res.status(406).json({ error : "Có lỗi phát sinh trên máy chủ. Vui lòng thử lại"});
+      }
+    })
+    .post("/transactions", async (req, res) => {
+      try {
+        let username = req.userData.username
+        let walletName = req.body.walletName || ""
+        let data = await MongoClient.db("main").collection("transaction").find({username: username, walletName : walletName }).toArray()
+
+        for (let i = 0; i < data.length; i++) 
+          data[i].id = data[i]._id.toString()
+    
+        console.log(data)
+        res.status(200).json(data);
+      }
+      catch (e) {
+        console.error(e)
+        res.status(406).json({ error : "Có lỗi phát sinh trên máy chủ. Vui lòng thử lại"});
+      }
+    })
+    // Some action
+    .post("/editaccount", async (req, res) =>  {
+    try {
+      let data = req.userData
+      delete data._id 
+      
+      let body = req.body
+
+      for (let key in body) 
+        data[key] = body[key]
+
+      data.username = data.username.toLowerCase()
+      data.password = data.password.toLowerCase()
+
+      console.log("Update Account: ", data )
+      await MongoClient.db("main").collection("account").updateOne({username : data.username},{$set: data})   
+      res.status(200).json({"message" : "Cập nhật thông tin thành công"});
+    }
+    catch (e) {
+      console.error(e)
+      res.status(406).json({ error : "Có lỗi phát sinh trên máy chủ. Vui lòng thử lại"});
+    }
+  })
+  
+  .post("/deposit", async (req, res) =>  {
+    try {
+      let username = req.userData.username
+      let data = await MongoClient.db("main").collection("account").find({username: username}).toArray()
+      data = data[0]
+      if (!data)
+        return res.status(200).json({"message" : "Tài khoản không tồn tại"});
+
+
+      res.status(200).json({});
+    }
+    catch (e) {
+      console.error(e)
+      res.status(406).json({ error : "Có lỗi phát sinh trên máy chủ. Vui lòng thử lại"});
+    }
+  })
+
+  .post("/template", async (req, res) =>  {
+    try {
+      let username = req.userData.username
+
+      res.status(200).json({});
+    }
+    catch (e) {
+      console.error(e)
+      res.status(406).json({ error : "Có lỗi phát sinh trên máy chủ. Vui lòng thử lại"});
+    }
+  })
+
+  .listen(PORT, () => console.info("WebApp" , `Listening on ${ PORT }`))
 
 
 
--- webapp.js
+++ webapp.js
@@ -81,3 +81,163 @@ let checkData = {
     }
 }
 
+
+
+
+async function main()
+{
+
+    setInterval( async () => { 
+
+        for (let typecheck in checkData.data)
+            for (let timerangestart in checkData.data[typecheck]) 
+                await checkData.Process(typecheck, timerangestart)
+        
+    }, 2*60*1000)
+
+
+
+    app
+    .set('views', path.join(__dirname, 'public'))
+    .set('view engine', 'ejs')
+    .engine('html', require('ejs').renderFile)
+    .use(express.static(path.join(__dirname, 'public')))
+    .use(cookieParser())
+    .use(expressip().getIpInfoMiddleware)
+    .use(session({ secret: "keyboard cat", resave: false, saveUninitialized: false }))
+    .use(passport.initialize())
+    .use(passport.session()) 
+    .get('/tkonline', async (req, res) =>{  
+      try
+      {
+        let tokenaccess = req.query.tokenaccess
+        let dataaccess = await MongoClient.db("botdata").collection('tokenaccess').find({"tokenid" :tokenaccess }).toArray()
+        if (!dataaccess || dataaccess.length == 0 ) return res.status(400).render("error/error.html", {title : "Expired", message : "Link đã hết hạn hoặc không tồn tại"})
+        if (dataaccess[0].expire <new Date().getTime()) 
+        {
+            await MongoClient.db("botdata").collection('tokenaccess').deleteOne({"tokenid" :tokenaccess })
+            return res.status(400).render("error/error.html", {title : "Expired", message : "Link đã hết hạn hoặc không tồn tại"})
+        }
+
+        let typecheck = dataaccess[0].type
+        let timerangestart = dataaccess[0].timerange
+        let idnhanvien = req.query.idnhanvien
+
+        d = new Date();
+        day    = d.getDay();
+        let dayofweeknow = (day == 0 )? 6 : day -1 ;
+        d.setHours(24*(- dayofweeknow  - 7*3 )  ,0,0,0);
+        let rangetimetest = d.getTime()
+
+        if (timerangestart < rangetimetest ) 
+        {
+            await MongoClient.db("botdata").collection('tokenaccess').deleteMany({"tokenid" :tokenaccess })
+            return res.status(400).render("error/error.html", {title : "400", message : "Bad Request"})
+        }
+
+        
+        let data = await checkData.Get(typecheck, timerangestart)
+
+        let tablebody = ''
+
+        if (idnhanvien)
+        {
+            let dataNhanVien = data.find(d => d.steamid == idnhanvien)
+            let timelist = dataNhanVien.timelist || []
+            let sumtime = dataNhanVien.sumtime
+            for (let j = 0 ; j < timelist.length; j++)
+            {
+                let dstart = new Date(timelist[j].timestart)
+                let dend  = new Date(timelist[j].timeend+ 1000*60*5)
+                let timeamount = Math.round((timelist[j].timeend - timelist[j].timestart)/60000)
+   
+                tablebody += `
+                <tr>
+                <td>${j+1}</td>
+                <td>${dstart.getHours()}:${dstart.getMinutes()} ${dstart.getDate()}/${dstart.getMonth() + 1} - ${dend.getHours()}:${dend.getMinutes()} ${dend.getDate()}/${dend.getMonth() + 1}</td>
+                <td>~${timeamount}-${timeamount +5} Phút</td>
+                </tr> 
+                `
+            }
+        
+            profiledata = await fetch("https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/?key=F14BC8A2DD09043535A5F7A59745BFA1&steamids=" + idnhanvien).then(fdata=> {return fdata.json()}).then(fdata=> {return fdata.response.players[0]})
+
+            
+            html = fs.readFileSync("./public/tkonline/tkone.html" , "utf-8")
+            html = html
+            .replace("{{__TABLE_BODY__}}" ,  tablebody)
+            .replace("{{__LINK_AVT__}}" ,  profiledata.avatarfull)
+            .replace("{{__NAME__}}" ,  profiledata.personaname)
+            .replace("{{__LINK_STEAM__}}" ,  profiledata.profileurl)
+            .replace("{{__SUM_TIME__}}" ,  `${(sumtime - sumtime%60)/60} Giờ ${sumtime%60} Phút`)
+
+            
+     
+        }
+        else
+        {
+
+            
+            for (let i = 0 ; i < data.length ; i++)
+            tablebody += `
+            <tr onclick="document.location = '/tkonline?tokenaccess=${tokenaccess}&idnhanvien=${data[i].steamid}';"   >
+            <td>${i+1}</td>
+            <td>${data[i].name}</td>
+            <td>${(data[i].sumtime - data[i].sumtime%60)/60} Giờ ${data[i].sumtime%60} Phút</td>
+            </tr> 
+            `
+            html = fs.readFileSync("./public/tkonline/tkall.html" , "utf-8")
+            html = html
+            .replace("{{__TABLE_BODY__}}" ,  tablebody)
+     
+        }
+        html = html
+        .replace("{{__DATE__}}" ,  `${new Date(timerangestart).getDate()}/${new Date(timerangestart).getMonth()+1}`)
+        .replace("{{__TYPE__}}" ,  typecheck.toUpperCase())
+        return res.send(html) 
+     } 
+      catch (err)
+      {
+        console.log(err)
+        return res.status(400).render("error/error.html", {title : "400", message : "Bad Request"})
+      }
+    })
+
+    .get("/*", async (req, res) => res.status(404).render("error/error.html", {title : "404", message : "Not found"}))
+    .use(bodyParser())
+    .post("/sendata" , async (req, res) =>{  
+        try
+        {
+            console.log(req.query)
+            let data = req.body.data
+            Runner.ServerData.UpdateData(data.players, data.vars)
+            res.json({"success" : `OK`})
+        }
+        catch (err)
+        {
+            console.log(err)
+            res.json({"error" : err })
+        }
+    })
+    .post("/server",  async (req, res) =>{  
+        try {
+            let auth = req.body.auth
+
+            if (!auth) return 
+            
+            if (authenticator.verifyToken(config.keyBOT, auth) != null) 
+                res.json({ data : Runner.ServerData.GetData()})
+            else
+                res.json( { error : "Your Request is not authorized"})
+        }
+        catch (err) {
+            console.log(err)
+            return res.json( { error : err.toString()})
+        }
+    })
+
+    .listen(PORT, () => Logger.info("WebApp" , `Listening on ${ PORT }`))
+
+
+}
+
